61f1257d6b9685f4e218e5bb78b068ac4fbd7af4
Carlos Vanzetto <vanzettoc@gmail.com>
2023-06-26 13:13:05 -0300

    fix: filter and select issues with one value

--- src/app/components/home/modal/modal.component.ts
+++ src/app/components/home/modal/modal.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { __values } from 'tslib';
 import { LoginService } from 'src/app/services/login/login.service';
 
@@ -16,8 +16,10 @@ export class ModalComponent implements OnInit {
 
   allCountries: string[] = [];
   filteredSize!: number;
+
   filter = new FormControl('');
-  selectCountry = new FormControl('');
+  selectCountry = new FormControl('', Validators.required);
+  selectedCountry!: string | null;
   @ViewChild('selectOptions') selectOptions!: any;
 
   onKeyUp(filterText: string | null): void {
@@ -31,19 +33,32 @@ export class ModalComponent implements OnInit {
       this.allCountries = [...this.countries]; // store all countries before filtering
     }
     if (filterText != null) {
+      console.log(filterText);
       this.countries = this.allCountries.filter((a) => a.toLowerCase().includes(filterText));
       this.filteredSize = this.countries.length;
+      console.log(this.countries);
+    }
+    if (this.countries.length === 1) {
+      this.selectCountry.setValue(this.countries[0]);
+      console.log(this.selectCountry.value);
     }
   }
-
+  onSelected() {
+    console.log(this.selectCountry.value);
+    this.selectedCountry = this.selectCountry.value;
+  }
   resize() {
     this.filteredSize = 1;
   }
-
-  selectedCountry: string = '';
+  cleanSelect() {
+    this.selectCountry.setValue(null);
+    this.filter.setValue('');
+  }
   constructor(private _loginService: LoginService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.selectCountry.valueChanges.subscribe(() => console.log(this.selectedCountry));
+  }
 
   getPais() {
     // if(!this._loginService.isLoggedIn){
@@ -51,6 +66,7 @@ export class ModalComponent implements OnInit {
     //   return
     //  } else {
     this.router.navigate([`/country/${this.selectedCountry}`]);
+    this.cleanSelect();
     //  }
   }
 }
