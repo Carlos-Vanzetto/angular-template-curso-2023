d4ca9691500abc91328a9f4e90ad2d54797b7565
DanteMiron <dantemiron@hotmail.com>
2023-07-04 11:11:00 -0300

    feat: country component update

--- src/app/components/home/modal/modal.component.ts
+++ src/app/components/home/modal/modal.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FormControl, Validators } from '@angular/forms';
 import { __values } from 'tslib';
 import { LoginService } from 'src/app/services/login/login.service';
+import { CountriesFlag } from 'src/app/models/countriesFlag';
 
 @Component({
   selector: 'app-modal',
@@ -10,16 +11,16 @@ import { LoginService } from 'src/app/services/login/login.service';
   styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
-  @Input() countries!: string[];
+  @Input() countries!: CountriesFlag[];
   @Input() modalTitle!: string;
   @Input() loading!: boolean;
 
-  allCountries: string[] = [];
+  allCountries: CountriesFlag[] = [];
   filteredSize!: number;
 
   filter = new FormControl('');
   selectCountry = new FormControl('', Validators.required);
-
+  selectedCountry!: string | null | undefined;
   @ViewChild('selectOptions') selectOptions!: any;
 
   onKeyUp(filterText: string | null): void {
@@ -32,38 +33,48 @@ export class ModalComponent implements OnInit {
     if (this.allCountries.length === 0) {
       this.allCountries = [...this.countries]; // store all countries before filtering
     }
-    if (filterText != null) {
-      console.log(filterText);
-      this.countries = this.allCountries.filter((a) => a.toLowerCase().includes(filterText));
-      this.filteredSize = this.countries.length;
-      console.log(this.countries);
-      console.log(this.selectCountry.value);
-    }
-    if (this.countries.length === 1) {
-      this.selectCountry.setValue(this.countries[0]);
-      console.log(this.selectCountry.value);
-    }
-  }
 
+
+    if (filterText != null) {
+      this.countries = this.allCountries.filter((a) => a.country.toLowerCase().includes(filterText));
+      this.filteredSize = this.countries.length;
+    }
+
+    this.selectCountry.setValue(null)
+  }
+  onSelected() {
+    console.log(this.selectCountry.value);
+    this.selectedCountry = this.selectCountry.value?.slice(5);
+    console.log(this.selectedCountry)
+  }
   resize() {
     this.filteredSize = 1;
   }
   cleanSelect() {
+    this.allCountries = [];
+    this.filteredSize = 1;
     this.selectCountry.setValue(null);
     this.filter.setValue('');
-    this.allCountries = [];
-    this.resize();
+  }
+
+  onFocus(){
+    if (this.countries.length === 1) {
+      this.selectCountry.setValue(this.countries[0].country);
+       console.log(this.selectCountry.value);
+       }
   }
   constructor(private _loginService: LoginService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.selectCountry.valueChanges.subscribe(() => console.log(this.selectedCountry));
+  }
 
   getPais() {
     // if(!this._loginService.isLoggedIn){
     //   this.router.navigate(['/login']);
     //   return
     //  } else {
-    this.router.navigate([`/country/${this.selectCountry.value}`]);
+    this.router.navigate([`/country/${this.selectedCountry}`]);
     this.cleanSelect();
     //  }
   }
---
+++ src/app/models/countriesFlag.ts
@@ -0,0 +1,4 @@
+export interface CountriesFlag {
+    country: string;
+    flag: string;
+}
